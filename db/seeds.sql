0349b14cfc1476656a4d73b93d63c48972c3ccb7
JasonPob <jasonpob@gmail.com>
2019-05-14 12:27:58 -0400

    all but index.hnadlebars

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Breakfast Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Avacado Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Baja Burger', false);
